a3a4ce6e1b3435053f48b29956ec7f7a38137b15
sophia <sophiamcar@gmail.com>
2026-04-19 16:56:52 -0400

    add to a text file

--- main.py
+++ main.py
@@ -14,20 +14,38 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
+# client.tree = tree
+
+
 
 
 #Add
 @tree.command(
-        name = "add_scheduled_ping",
-        description = "Add a time to ping everyone in format: <Mon/Tues/Wed/Thu/Fri/Sat/Sun>, <Time of day AM/PM>, <role>"
+    name = "add_scheduled_ping",
+    description = "Add a time to ping everyone in format: <Mon/Tues/Wed/Thu/Fri/Sat/Sun>, <Time of day AM/PM>, <role>"
 )
-async def add_to_schedule(interaction: discord.Interaction, message: str):
+async def add_to_schedule(interaction: discord.Interaction, message: str) -> None:
     message.strip("")
     info = message.split(",")
     day = info[0]
     time = info[1]
     role = info[2]
+    f = open("schedule.txt", "a")
+    f.write(message)
+    f.write("\n")
+    f.close()
+    await interaction.response.send_message(message)
     print(day + " " + time + " " + role)
+
+
+
+# #Print schedule
+# @tree.command(
+#     name = "print_schedule",
+#     description = "whats the schedule?"
+# )
+# async def add_to_schedule(interaction: discord.Interaction) -> None:
+#     await interaction.response.send_message("schedule")
 
 
 ##MAIN##
@@ -41,3 +59,4 @@ async def on_ready():
 f = open("token.txt", "r")
 token = f.readline().strip("\n");
 client.run(token)
+f.close()
