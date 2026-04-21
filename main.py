b57e7b6bece19915a803c466b7ba41004ad1b913
sophia <sophiamcar@gmail.com>
2026-04-20 20:39:06 -0400

    added day parsing

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ from datetime import date
 import discord
 from discord import app_commands
 
-# CHANNEL = #add in channel here
-# TOKEN = #add in here
-
 #setting up discord client
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
-# client.tree = tree
+
+#hoping to put it in a list because it shouldnt be that long for my server purposes. Can move to a file
+schedule = []
+
+day_map = {"SUN":1, "MON":2, "TUE": 3, "WEN": 4, "THU": 5, "FRI": 6, "SAT": 7}
 
 
+########## HELPER FUNCTIONS ###########
 
+#Parse the day string
+#Input: day as a str
+#Returns: day as a number or zero for invalid input
+def parse_day(day: str):
+    day = day.upper()
+    day = day[:3]
+    print(day)
+    return day_map.get(day, 0)
+    
 
+######### DISCORD FUNCTIONS ###########
 #Add
 @tree.command(
     name = "add_scheduled_ping",
@@ -27,16 +39,23 @@ tree = app_commands.CommandTree(client)
 async def add_to_schedule(interaction: discord.Interaction, message: str) -> None:
     message.strip("")
     info = message.split(",")
-    day = info[0]
-    time = info[1]
-    role = info[2]
-    f = open("schedule.txt", "a")
-    f.write(message)
-    f.write("\n")
-    f.close()
-    await interaction.response.send_message(message)
-    print(day + " " + time + " " + role)
 
+    day = info[0] #TODO condense later
+    day = parse_day(day)
+    if (day == 0):
+        await interaction.response.send_message("invalid input")
+        return
+    else:
+        info[0] = day
+    
+    time = info[1]
+    
+    role = info[2]
+    
+    schedule.append(info)
+    
+    await interaction.response.send_message("valid, thank you")
+    print(schedule)
 
 
 # #Print schedule
