9851d6db04d9142b371525a8181e548b9bb35f51
sophia <sophiamcar@gmail.com>
2026-04-26 16:22:00 -0400

    time parsing

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ tree = app_commands.CommandTree(client)
 #hoping to put it in a list because it shouldnt be that long for my server purposes. Can move to a file
 schedule = []
 
-day_map = {"SUN":1, "MON":2, "TUE": 3, "WEN": 4, "THU": 5, "FRI": 6, "SAT": 7}
+day_map = {"SUN":1, "MON":2, "TUE": 3, "WED": 4, "THU": 5, "FRI": 6, "SAT": 7}
 
 
 ########## HELPER FUNCTIONS ###########
@@ -26,20 +26,45 @@ day_map = {"SUN":1, "MON":2, "TUE": 3, "WEN": 4, "THU": 5, "FRI": 6, "SAT": 7}
 def parse_day(day: str):
     day = day.upper()
     day = day[:3]
-    print(day)
     return day_map.get(day, 0)
+
+#-------------------------------------------------------
+
+def parse_time(time: str):
+    time.lower();
+    time.strip(" ")
+    #get if its am or pm 
+    afternoon = False
+    if (time.find("am")):
+        afternoon = False
+    elif (time.find("pm")):
+        afternoon = True
+    else:
+        return -1, -1
+    time.rstrip("apm")
+    
+    #if contains a : then separate hours from minutes
+    info = time.split(":", 1)
+    hours = info[0]
+    minutes = 0
+    if (len(info) == 2):
+        minutes = info[1]
+    print(hours + " " + minutes)
+    return hours, minutes
+
     
 
 ######### DISCORD FUNCTIONS ###########
 #Add
 @tree.command(
     name = "add_scheduled_ping",
-    description = "Add a time to ping everyone in format: <Mon/Tues/Wed/Thu/Fri/Sat/Sun>, <Time of day AM/PM>, <role>"
+    description = "format: <Mon/Tues/Wed/Thu/Fri/Sat/Sun>, <Time of day AM/PM>, <role>, <message>"
 )
 async def add_to_schedule(interaction: discord.Interaction, message: str) -> None:
     message.strip("")
     info = message.split(",")
 
+    #parse day
     day = info[0] #TODO condense later
     day = parse_day(day)
     if (day == 0):
@@ -48,9 +73,15 @@ async def add_to_schedule(interaction: discord.Interaction, message: str) -> Non
     else:
         info[0] = day
     
-    time = info[1]
+    #parse time
+    time = parse_time(info[1])
+    if (time[0] == -1):
+        await interaction.response.send_message("invalid input")
+        return
     
     role = info[2]
+
+    mess = info[3]
     
     schedule.append(info)
     
